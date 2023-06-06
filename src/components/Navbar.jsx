a06bd551fe8e2f681e364ea2018fa1b68e02649d
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-06-06 18:32:29 +0530

    Added icons of home in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import "../Styles/Navbar.css";
+import home from "../assets/home.png";
 
 const Navbar = () => {
   return (
     <div className="nav">
       <div>Logo</div>
-      <div>Home</div>
+      <div>
+        <img src={home} alt="" className="socials" />
+      </div>
       <div>Profile</div>
     </div>
   );
