f8fb656ee90cada0312ab3c1c150c8a37afff822
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-06-06 18:33:31 +0530

    Minor Changes

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./Navbar";
 import "../Styles/HomPage.css";
 import Hero from "./Hero";
+import Test from "./Test";
 
 const HomePage = () => {
   return (
     <div className="home">
       <Navbar />
       <Hero/>
+      {/* <Test /> */}
     </div>
   );
 };
---
+++ src/components/Test.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const Test = () => {
+  return (
+    <Fragment>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+    </Fragment>
+  );
+};
+
+export default Test;
