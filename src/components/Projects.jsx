699cdfd287459741e64ddedfd6e752845e3530ba
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-07-16 19:51:06 +0530

    Created a parent component of Project component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,15 @@
+import '../Styles/Projects.css'
+import Project from './Project';
+
+const Projects = () => {
+  return (
+    <div className="project__body">
+      <div className="project_container" id="projects">
+        <span>Projects</span>
+      </div>
+      <Project />
+    </div>
+  );
+};
+
+export default Projects;
