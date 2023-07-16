55d0e28f32b31656a0cded81e2f7590f05202048
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-07-16 19:49:45 +0530

    Created and imported skills section component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,22 @@
+import "../Styles/Skill.css";
+
+const Skills = () => {
+  return (
+    <div className="skill__body">
+      <div className="skill_wrapper">
+        <span className="skill">HTML</span>
+        <span className="skill">CSS</span>
+        <span className="skill">JavaScript</span>
+        <span className="skill">React</span>
+        <span className="skill">Redux-Toolkit</span>
+        <span className="skill">Tailwind CSS</span>
+        <span className="skill">Bootstrap</span>
+        <span className="skill">Express</span>
+        <span className="skill">Node</span>
+        <span className="skill">MongoDB</span>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
