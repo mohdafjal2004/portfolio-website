c495e77819766c48f81970c03ec3026b96b4c011
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-07-16 19:51:59 +0530

    Finished  first version of this app using footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../Styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer_style">Made with ❤️ by  <span className="name_style">Mohd Afjal</span></div>
+    </div>
+  );
+};
+
+export default Footer;
